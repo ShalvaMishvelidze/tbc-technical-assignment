f908c4ab5e189785288cf97943c6fe88a6b8a02c
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-01-23 21:36:49 +0400

    loaded fonts with webpack

--- webpack.common.js
+++ webpack.common.js
@@ -45,6 +45,10 @@ module.exports = {
         test: /\.html$/,
         use: ["html-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
